1389100e9a5967f879c03abe1cbe7620102f4b90
Khachatur Yeritsyan <khachatur.yeritsyan@russianschool.com>
2025-02-27 11:40:42 +0400

    Export IgnorePaths.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 mod recursive_bindgen;
 mod recursive_compile;
 mod recursive_linker;
-pub mod command_line_arguments;
 mod ignore_paths;
+pub mod command_line_arguments;
 
 pub use crate::recursive_bindgen::generate_module_bindings;
 pub use crate::recursive_compile::{compile_to_static_libs, CompilationOptions};
 pub use crate::recursive_linker::{get_static_libraries};
+pub use crate::ignore_paths::IgnorePaths;
 pub use bindgen::Builder as BindgenBuilder;
